5b2896824c21939e48efd613a2c1deaa9b517149
BWTESTs <narisorn14@gmail.com>
2017-05-03 10:42:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,10 +19,8 @@ if (!is_null($events['events'])) {
 			if (strpos($text, 'สเต็ก') !== false) {
 				// Build message to reply back
 				$messages = [
-					{
-						'type' => 'text',
-						'text' => '1.ร้านสเต็ก...'
-					}
+					'type' => 'text',
+					'text' => '1.ร้านสเต็ก...'
 				];
 			}
 			else { 
