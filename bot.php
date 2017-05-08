9099a96d9d314bf4b3ea21459fdef2d965d61d68
BWTESTs <narisorn14@gmail.com>
2017-05-08 18:47:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,10 +10,10 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		
+
 		$replyToken = $event['replyToken'];
-		
-		if ($event['message']['type'] == 'location')
+
+		if ($event['type'] == 'message' && $event['message']['type'] == 'location')
 		{
 			$messages = [
 				'type' => 'text',
@@ -23,6 +23,24 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+
+			$post = json_encode($data);
+
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		}
 		else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
